b0709116b61a24254609796a0e7a703328540444
JwowSquared <Jdwagner1@hotmail.com>
2020-11-03 18:45:05 +0000

    put dummy tests in the files

---
+++ tests/test_base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""x"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """x"""
+    def test_blank(self):
+        """x"""
+        self.assertEqual(1, 1)
