13851bb5be6666fe54e3550316c64040b65c30b2
1819@holbertonschool.com <1819@holbertonschool.com>
2020-11-04 17:39:18 -0600

    adding class City for task 9

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that contains the class City
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """representation of the class city"""
+    state_id = ""
+    name = ""
