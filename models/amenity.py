7d3fcf67cbd9403b0464951a37cd3ee70b05c9dd
1819@holbertonschool.com <1819@holbertonschool.com>
2020-11-04 17:43:51 -0600

    adding amenity for task 9

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module that contains the class Amenity
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Representation of the class Amenity"""
+    name = ""
